4b3062892b2526be9099537a8a0f84602fc37aa6
EandroC <eandrocleiton@gmail.com>
2022-04-16 08:20:42 -0300

    Navbar e footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
-import Company from './components/Company'
-import Contact from './components/Contact'
-import Home from './components/Home'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import Home from './components/pages/Home'
 import Container from './components/layout/Container'
-import NewProject from './components/NewProject'
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+import NewProject from './components/pages/NewProject'
+import Projects from './components/pages/Projects'
 
 function App() {
 
 
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/company">Company</Link>
-        <Link to="/newproject">NewProject</Link>
-      </div>
+      <Navbar/>
       <Container customClass="min-height">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/company" element={<Company />} />
           <Route path="/newproject" element={<NewProject />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </Container>
+      <Footer/>
     </Router>
   )
 }
